9b485bde7bf6eed221080aca6a4a72a8cf35c9f7
CADBooster-Peter <peter@cadbooster.com>
2025-04-12 20:38:41 +0200

    Fixed example project after enum rename.  Added more comments to explain the intricacies of the different kind of menus.

--- Tutorials/09-CommandItems/SolidDna.CommandItems/CommandItems.cs
+++ Tutorials/09-CommandItems/SolidDna.CommandItems/CommandItems.cs
@@ -1,7 +1,6 @@
 ﻿using CADBooster.SolidDna;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using static CADBooster.SolidDna.SolidWorksEnvironment;
@@ -39,6 +38,7 @@ namespace SolidDna.CommandItems
     public class MySolidDnaPlugin : SolidPlugIn
     {
         #region Public Properties
+
         /// <summary>
         /// My Add-in description
         /// </summary>
@@ -60,9 +60,22 @@ namespace SolidDna.CommandItems
 
         public override void ConnectedToSolidWorks()
         {
+            CreateMenus();
+        }
+
+        public override void DisconnectedFromSolidWorks()
+        {
+
+        }
+
+        /// <summary>
+        /// Create a toolbar with a flyout, separators and other items, plus a Tools menu.
+        /// </summary>
+        private void CreateMenus()
+        {
             var imageFormat = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "icons{0}.png");
 
-            // FlyoutGroup
+            // Create a flyout (group) with a list of items
             var flyout = Application.CommandManager.CreateFlyoutGroup2(
                 title: "CreateFlyoutGroup2 Example",
                 CreateCommandItems(),
@@ -74,19 +87,17 @@ namespace SolidDna.CommandItems
                 CommandManagerFlyoutType.FirstItemVisible
             );
 
-            // CommandTab
+            // Create a command group / command tab / toolbar 
+            var commandManagerItems = CreateCommandItemsForToolbar(flyout);
+            
             Application.CommandManager.CreateCommandTab(
                 title: "CreateCommandTab Example",
                 id: 150_000,
-                commandManagerItems: CreateCommandItems()
-                    .Cast<ICommandManagerItem>()
-                    // Append flyout
-                    .Append(flyout)
-                    .ToList(),
+                commandManagerItems: commandManagerItems,
                 mainIconPathFormat: imageFormat,
                 iconListsPathFormat: imageFormat);
 
-            // CommandMenu
+            // Create a command menu / Tools menu 
             Application.CommandManager.CreateCommandMenu(
                 title: "CreateCommandMenu Example",
                 id: 150_001,
@@ -94,84 +105,183 @@ namespace SolidDna.CommandItems
         }
 
         /// <summary>
-        /// Initialize command items to reuse them in example
+        /// Create a list of command items for a toolbar, Tools menu and a flyout. Create this list for each menu so its IDs are unique.
         /// </summary>
-        /// <returns>CommandManagerItems</returns>
         public List<CommandManagerItem> CreateCommandItems() =>
-            new List<CommandManagerItem>{
-                    new CommandManagerItem {
-                        Name = "DeselectedDisabled item",
-                        Tooltip = "DeselectedDisabled item Tooltip",
-                        ImageIndex = 0,
-                        Hint = "DeselectedDisabled item Hint",
-                        VisibleForDrawings = true,
-                        VisibleForAssemblies = true,
-                        VisibleForParts = true,
-                        OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab DeselectedDisabled item clicked!"),
-                        OnStateCheck = (args) => args.Result = ItemState.DeselectedDisabled
-                    },
-                    new CommandManagerItem {
-                        Name = "DeselectedEnabled item",
-                        Tooltip = "DeselectedEnabled item Tooltip",
-                        ImageIndex = 1,
-                        Hint = "DeselectedEnabled item Hint",
-                        VisibleForDrawings = true,
-                        VisibleForAssemblies = true,
-                        VisibleForParts = true,
-                        OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab DeselectedEnabled item clicked!"),
-                        OnStateCheck = (args) => args.Result = ItemState.DeselectedEnabled
-                    },
-                    new CommandManagerItem {
-                        Name = "SelectedDisabled item",
-                        Tooltip = "SelectedDisabled item Tooltip",
-                        ImageIndex = 2,
-                        Hint = "SelectedDisabled item Hint",
-                        VisibleForDrawings = true,
-                        VisibleForAssemblies = true,
-                        VisibleForParts = true,
-                        OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab SelectedDisabled item clicked!"),
-                        OnStateCheck = (args) => args.Result = ItemState.SelectedDisabled
-                    },
-                    new CommandManagerItem {
-                        Name = "SelectedEnabled item",
-                        Tooltip = "SelectedEnabled item Tooltip",
-                        ImageIndex = 0,
-                        Hint = "SelectedEnabled item Hint",
-                        VisibleForDrawings = true,
-                        VisibleForAssemblies = true,
-                        VisibleForParts = true,
-                        OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab SelectedEnabled item clicked!"),
-                        OnStateCheck = (args) => args.Result = ItemState.SelectedEnabled
-                    },
-                    new CommandManagerItem {
-                        Name = "Hidden item",
-                        Tooltip = "Hidden item Tooltip",
-                        ImageIndex = 1,
-                        Hint = "Hidden item Hint",
-                        VisibleForDrawings = true,
-                        VisibleForAssemblies = true,
-                        VisibleForParts = true,
-                        OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab Hidden item clicked!"),
-                        OnStateCheck = (args) => args.Result = ItemState.Hidden
-                    },
-                    new CommandManagerItem {
-                        Name = "Toggle item",
-                        Tooltip = "Toggle item Tooltip",
-                        ImageIndex = 2,
-                        Hint = "Toggle item Hint",
-                        VisibleForDrawings = true,
-                        VisibleForAssemblies = true,
-                        VisibleForParts = true,
-                        OnClick = () => mToggle = !mToggle,
-                        OnStateCheck = (args) =>
-                            args.Result = mToggle ?  ItemState.SelectedEnabled : ItemState.DeselectedEnabled
-                    }
-                };
+            new List<CommandManagerItem>
+            {
+                new CommandManagerItem
+                {
+                    Name = "DeselectedDisabled item",
+                    Tooltip = "DeselectedDisabled item Tooltip",
+                    ImageIndex = 0,
+                    Hint = "DeselectedDisabled item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab DeselectedDisabled item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.DeselectedDisabled
+                },
+                new CommandManagerItem
+                {
+                    Name = "DeselectedEnabled item",
+                    Tooltip = "DeselectedEnabled item Tooltip",
+                    ImageIndex = 1,
+                    Hint = "DeselectedEnabled item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab DeselectedEnabled item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.DeselectedEnabled
+                },
 
-        public override void DisconnectedFromSolidWorks()
-        {
+                new CommandManagerItem
+                {
+                    Name = "SelectedDisabled item",
+                    Tooltip = "SelectedDisabled item Tooltip",
+                    ImageIndex = 2,
+                    Hint = "SelectedDisabled item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab SelectedDisabled item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.SelectedDisabled
+                },
+                new CommandManagerItem
+                {
+                    Name = "SelectedEnabled item",
+                    Tooltip = "SelectedEnabled item Tooltip",
+                    ImageIndex = 0,
+                    Hint = "SelectedEnabled item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab SelectedEnabled item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.SelectedEnabled
+                },
+                new CommandManagerItem
+                {
+                    Name = "Hidden item",
+                    Tooltip = "Hidden item Tooltip",
+                    ImageIndex = 1,
+                    Hint = "Hidden item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab Hidden item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.Hidden
+                },
+                new CommandManagerItem
+                {
+                    Name = "Toggle item",
+                    Tooltip = "Toggle item Tooltip",
+                    ImageIndex = 2,
+                    Hint = "Toggle item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => mToggle = !mToggle,
+                    OnStateCheck = (args) =>
+                        args.Result = mToggle ? CommandManagerItemState.SelectedEnabled : CommandManagerItemState.DeselectedEnabled
+                }
+            };
 
-        }
+        /// <summary>
+        /// Create a list of command items for a toolbar. A toolbar can hold normal items (buttons), flyouts and separators, all of which implement ICommandManagerItem.
+        /// So the reuse the list of command items from this example, we cast them to ICommandManagerItem first.
+        /// Add a separator and a flyout
+        /// </summary>
+        /// <param name="flyout"></param>
+        public List<ICommandManagerItem> CreateCommandItemsForToolbar(CommandManagerFlyout flyout) =>
+            new List<ICommandManagerItem>
+            {
+                // Add the flyout (group), which also contains a list of items
+                flyout,
+
+                // Add a separator
+                new CommandManagerSeparator(),
+                
+                // Add a list of items
+                new CommandManagerItem
+                {
+                    Name = "DeselectedDisabled item",
+                    Tooltip = "DeselectedDisabled item Tooltip",
+                    ImageIndex = 0,
+                    Hint = "DeselectedDisabled item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab DeselectedDisabled item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.DeselectedDisabled
+                },
+                new CommandManagerItem
+                {
+                    Name = "DeselectedEnabled item",
+                    Tooltip = "DeselectedEnabled item Tooltip",
+                    ImageIndex = 1,
+                    Hint = "DeselectedEnabled item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab DeselectedEnabled item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.DeselectedEnabled
+                },
+
+                new CommandManagerSeparator(),
+
+                new CommandManagerItem
+                {
+                    Name = "SelectedDisabled item",
+                    Tooltip = "SelectedDisabled item Tooltip",
+                    ImageIndex = 2,
+                    Hint = "SelectedDisabled item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab SelectedDisabled item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.SelectedDisabled
+                },
+                new CommandManagerItem
+                {
+                    Name = "SelectedEnabled item",
+                    Tooltip = "SelectedEnabled item Tooltip",
+                    ImageIndex = 0,
+                    Hint = "SelectedEnabled item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab SelectedEnabled item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.SelectedEnabled
+                },
+
+                new CommandManagerSeparator(),
+
+                new CommandManagerItem
+                {
+                    Name = "Hidden item",
+                    Tooltip = "Hidden item Tooltip",
+                    ImageIndex = 1,
+                    Hint = "Hidden item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => System.Windows.MessageBox.Show("CreateCommandTab Hidden item clicked!"),
+                    OnStateCheck = (args) => args.Result = CommandManagerItemState.Hidden
+                },
+                new CommandManagerItem
+                {
+                    Name = "Toggle item",
+                    Tooltip = "Toggle item Tooltip",
+                    ImageIndex = 2,
+                    Hint = "Toggle item Hint",
+                    VisibleForDrawings = true,
+                    VisibleForAssemblies = true,
+                    VisibleForParts = true,
+                    OnClick = () => mToggle = !mToggle,
+                    OnStateCheck = (args) =>
+                        args.Result = mToggle ? CommandManagerItemState.SelectedEnabled : CommandManagerItemState.DeselectedEnabled
+                }
+            };
 
         #endregion
     }
