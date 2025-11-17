6c58d9dc2428524a013e2292e6d77ae01a67e939
CADBooster-Peter <peter@cadbooster.com>
2025-11-17 15:35:37 +0100

    Cleaned up the addin installer project. Renamed the output file.

--- Tools/Addin Installer/ValueConverters/PathToFileNameConverter.cs
+++ Tools/Addin Installer/ValueConverters/PathToFileNameConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.IO;
 using System.Windows.Data;
 
-namespace AngelSix.SolidWorksApi.AddinInstaller
+namespace AngelSix.SolidWorksApi.AddinInstaller.ValueConverters
 {
     public class PathToFileNameConverter : IValueConverter
     {
--- Tools/Addin Installer/ValueConverters/PathToVisibilityConverter.cs
+++ Tools/Addin Installer/ValueConverters/PathToVisibilityConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
-namespace AngelSix.SolidWorksApi.AddinInstaller
+namespace AngelSix.SolidWorksApi.AddinInstaller.ValueConverters
 {
     [ValueConversion(typeof(string), typeof(Visibility))]
     public class PathToVisibilityConverter: IValueConverter
