1338da99ccb36cbac074a130bf124d110c2ead29
CADBooster-Peter <peter@cadbooster.com>
2025-12-01 18:05:15 +0100

    Added enumerating of the selected objects

--- SolidDna/SolidWorks/SelectionManager/SelectionManager.cs
+++ SolidDna/SolidWorks/SelectionManager/SelectionManager.cs
@@ -25,24 +25,52 @@ namespace CADBooster.SolidDna
         #region Selected Entities
 
         /// <summary>
+        /// Enumerate through all selected objects in the current model with a certain selection mark.
+        /// Allows you to stop enumerating early if needed.
+        /// </summary>
+        /// <param name="selectionMark">Only include objects with this selection mark</param>
+        /// <returns>An enumerable of selected objects</returns>
+        public IEnumerable<SelectedObject> EnumerateSelectedObjects(SelectionMark selectionMark = SelectionMark.Any)
+        {
+            // Get selection count
+            var count = GetSelectedObjectCount(selectionMark);
+
+            // If we have none, we are done
+            if (count <= 0)
+                yield break;
+
+            // Otherwise, get all selected objects
+            for (var i = 1; i <= count; i++)
+            {
+                // Get the object and its type.
+                var selected = GetSelectedObjectAtIndex(i, selectionMark);
+
+                // Return it
+                yield return selected;
+            }
+        }
+
+        /// <summary>
         /// Get the selected object at a given index and with a certain selection mark. Index is 1-based.
         /// </summary>
-        /// <param name="index"></param>
-        /// <param name="selectionMark"></param>
+        /// <param name="index">The index of the selected object. Is 1-based.</param>
+        /// <param name="selectionMark">Only include objects with this selection mark</param>
         /// <returns></returns>
-        public SelectedObject GetSelectedObjectAtIndex(int index, SelectionMark selectionMark = SelectionMark.Any) => 
+        public SelectedObject GetSelectedObjectAtIndex(int index, SelectionMark selectionMark = SelectionMark.Any) =>
             new SelectedObject(BaseObject.GetSelectedObject6(index, (int)selectionMark), (swSelectType_e)BaseObject.GetSelectedObjectType3(index, (int)selectionMark));
 
         /// <summary>
         /// Get the number of selected objects in the current model with a certain selection mark.
         /// </summary>
-        /// <param name="mark"></param>
+        /// <param name="mark">Only include objects with this selection mark</param>
         /// <returns></returns>
         public int GetSelectedObjectCount(SelectionMark mark = SelectionMark.Any) => BaseObject.GetSelectedObjectCount2((int)mark);
 
         /// <summary>
         /// Get all selected objects in the current model with a certain selection mark.
         /// </summary>
+        /// <param name="selectionMark">Only include objects with this selection mark</param>
+        /// <returns>A list of all selected objects with the specified selection mark</returns>
         public List<SelectedObject> GetSelectedObjects(SelectionMark selectionMark = SelectionMark.Any)
         {
             // Create list
