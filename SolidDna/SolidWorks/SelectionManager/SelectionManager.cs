290d300146c7f687f79d0051d61ac23a4d5ee4e0
CADBooster-Peter <peter@cadbooster.com>
2025-12-01 16:48:05 +0100

    Added getting list of selected objects, selected object at index and selected object count. All with Marks.

--- SolidDna/SolidWorks/SelectionManager/SelectionManager.cs
+++ SolidDna/SolidWorks/SelectionManager/SelectionManager.cs
@@ -25,23 +25,67 @@ namespace CADBooster.SolidDna
         #region Selected Entities
 
         /// <summary>
-        /// Gets all the selected objects in the model
+        /// Get the selected object at a given index and with a certain selection mark. Index is 1-based.
         /// </summary>
-        /// <param name="action">The selected objects list to be worked on inside the action.
-        ///     NOTE: Do not store references to them outside of this action</param>
+        /// <param name="index"></param>
+        /// <param name="selectionMark"></param>
         /// <returns></returns>
-        public void SelectedObjects(Action<List<SelectedObject>> action)
+        public SelectedObject GetSelectedObjectAtIndex(int index, SelectionMark selectionMark = SelectionMark.Any) => 
+            new SelectedObject(BaseObject.GetSelectedObject6(index, (int)selectionMark), (swSelectType_e)BaseObject.GetSelectedObjectType3(index, (int)selectionMark));
+
+        /// <summary>
+        /// Get the number of selected objects in the current model with a certain selection mark.
+        /// </summary>
+        /// <param name="mark"></param>
+        /// <returns></returns>
+        public int GetSelectedObjectCount(SelectionMark mark = SelectionMark.Any) => BaseObject.GetSelectedObjectCount2((int)mark);
+
+        /// <summary>
+        /// Get all selected objects in the current model with a certain selection mark.
+        /// </summary>
+        public List<SelectedObject> GetSelectedObjects(SelectionMark selectionMark = SelectionMark.Any)
         {
             // Create list
             var list = new List<SelectedObject>();
 
-            // Get selected objects with any mark value
-            const int anyMark = -1;
+            // Get selection count
+            var count = GetSelectedObjectCount(selectionMark);
+
+            // Get all selected objects
+            for (var i = 1; i <= count; i++)
+            {
+                // Get the object and its type. Add it to the list.
+                list.Add(GetSelectedObjectAtIndex(i, selectionMark));
+            }
+
+            // Return the list
+            return list;
+        }
+
+        /// <summary>
+        /// Perform an action on all selected objects in the current model with any selection mark.
+        /// </summary>
+        /// <param name="action">The selected objects list to be worked on inside the action.
+        /// NOTE: Do not store references to these objects outside of this action</param>
+        /// <returns></returns>
+        public void SelectedObjects(Action<List<SelectedObject>> action) => SelectedObjects(action, SelectionMark.Any);
+
+        /// <summary>
+        /// Perform an action on all selected objects in the current model with a certain selection mark.
+        /// </summary>
+        /// <param name="action">The selected objects list to be worked on inside the action.
+        /// NOTE: Do not store references to these objects outside of this action</param>
+        /// <param name="selectionMark"></param>
+        /// <returns></returns>
+        public void SelectedObjects(Action<List<SelectedObject>> action, SelectionMark selectionMark)
+        {
+            // Create list
+            var list = new List<SelectedObject>();
 
             try
             {
                 // Get selection count
-                var count = BaseObject.GetSelectedObjectCount2(anyMark);
+                var count = GetSelectedObjectCount(selectionMark);
 
                 // If we have none, we are done
                 if (count <= 0)
@@ -53,11 +97,8 @@ namespace CADBooster.SolidDna
                 // Get all selected objects
                 for (var i = 1; i <= count; i++)
                 {
-                    // Get the object and its type
-                    var selected = new SelectedObject(BaseObject.GetSelectedObject6(i, anyMark), (swSelectType_e)BaseObject.GetSelectedObjectType3(i, anyMark));
-
-                    // Add to the list
-                    list.Add(selected);
+                    // Get the object and its type. Add it to the list.
+                    list.Add(GetSelectedObjectAtIndex(i, selectionMark));
                 }
 
                 // Call the action
@@ -68,7 +109,6 @@ namespace CADBooster.SolidDna
                 // Dispose of all items
                 list.ForEach(f => f.Dispose());
             }
-
         }
 
         #endregion
