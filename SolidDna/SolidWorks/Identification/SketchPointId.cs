f59c02af6b119d94b8c11749c323dddd3eea87dc
CADBooster-Peter <peter@cadbooster.com>
2025-12-15 21:44:13 +0100

    Created a separate SketchPointId and removed the hack to use SketchSegmentId for points.

--- SolidDna/SolidWorks/Identification/SketchPointId.cs
+++ SolidDna/SolidWorks/Identification/SketchPointId.cs
@@ -6,13 +6,11 @@ using SolidWorks.Interop.sldworks;
 namespace CADBooster.SolidDna
 {
     /// <summary>
-    /// The unique ID for a sketch segment. Consists of two longs, but is only unique in combination with the sketch (name) and sketch segment type.
-    /// This means that the same two long values can be used in different sketches and the same sketch can have segments with the same two long values but different types.
-    /// See <see href="https://help.solidworks.com/2026/english/api/sldworksapi/solidworks.interop.sldworks~solidworks.interop.sldworks.isketchsegment~getid.html"/>.
-    /// Sketch points are not sketch segments and cannot be cast to <see cref="SketchSegment"/>.
-    /// Sketch points also have two long values and a sketch name, so use <see cref="SketchPointId"/> for those.
+    /// The unique ID for a sketch point. Consists of two longs, but is only unique in combination with the sketch (name).
+    /// This means that the same two long values can be used in different sketches. Use <see cref="SketchSegmentId"/> for sketch segments.
+    /// See <see href="https://help.solidworks.com/2026/english/api/sldworksapi/solidworks.interop.sldworks~solidworks.interop.sldworks.isketchpoint~getid.html"/>.
     /// </summary>
-    public class SketchSegmentId
+    public class SketchPointId
     {
         #region Public properties
 
@@ -31,32 +29,24 @@ namespace CADBooster.SolidDna
         /// </summary>
         public string SketchName { get; }
 
-        /// <summary>
-        /// Sketch segment type. 
-        /// </summary>
-        public SketchSegmentType Type { get; }
-
         #endregion
 
         #region Constructor
 
         /// <summary>
-        /// The unique identifier for a sketch segment.
-        /// Is determined by its sketch name, two long/int values and the type.
+        /// The unique identifier for a point in a sketch.
+        /// Is determined by its sketch name, two long/int values.
         /// </summary>
-        /// <param name="sketchSegment"></param>
-        public SketchSegmentId(ISketchSegment sketchSegment)
+        /// <param name="sketchPoint"></param>
+        public SketchPointId(ISketchPoint sketchPoint)
         {
             // Get the two longs 
-            var ids = GetIds(sketchSegment);
+            var ids = GetIds(sketchPoint);
             Id0 = ids[0];
             Id1 = ids[1];
 
             // Get the sketch name by casting the sketch to a Feature first
-            SketchName = ((Feature)sketchSegment.GetSketch()).Name;
-
-            // Get the sketch segment type
-            Type = (SketchSegmentType)sketchSegment.GetType();
+            SketchName = ((Feature)sketchPoint.GetSketch()).Name;
         }
 
         #endregion
@@ -64,25 +54,25 @@ namespace CADBooster.SolidDna
         #region Equals, GetHashCode and ToString
 
         // Inheritdoc
-        public override string ToString() => $"Sketch segment ID {SketchName}-{Type}-{Id0}-{Id1}";
+        public override string ToString() => $"Sketch Point ID {SketchName}-{Id0}-{Id1}";
 
         /// <summary>
-        /// Get if this sketch segment ID is equal to another sketch segment ID.
+        /// Get if this sketch point ID is equal to another sketch point ID.
         /// </summary>
         /// <param name="otherId"></param>
         /// <returns></returns>
-        public bool Equals(SketchSegmentId otherId)
+        public bool Equals(SketchPointId otherId)
         {
-            return SketchName.Equals(otherId.SketchName, StringComparison.InvariantCultureIgnoreCase) && Id0 == otherId.Id0 && Id1 == otherId.Id1 && Type == otherId.Type;
+            return SketchName.Equals(otherId.SketchName, StringComparison.InvariantCultureIgnoreCase) && Id0 == otherId.Id0 && Id1 == otherId.Id1;
         }
 
         // Inheritdoc
         public override bool Equals(object obj)
         {
-            if (obj == null || obj.GetType() != typeof(SketchSegmentId))
+            if (obj == null || obj.GetType() != typeof(SketchPointId))
                 return false;
 
-            return Equals((SketchSegmentId)obj);
+            return Equals((SketchPointId)obj);
         }
 
         // Inheritdoc
@@ -93,7 +83,6 @@ namespace CADBooster.SolidDna
                 var hashCode = SketchName != null ? SketchName.GetHashCode() : 0;
                 hashCode = (hashCode * 397) ^ Id0.GetHashCode();
                 hashCode = (hashCode * 397) ^ Id1.GetHashCode();
-                hashCode = (hashCode * 397) ^ (int)Type;
                 return hashCode;
             }
         }
@@ -106,19 +95,19 @@ namespace CADBooster.SolidDna
         /// Get two longs from two integers or longs.
         /// See https://help.solidworks.com/2026/english/api/sldworksapiprogguide/overview/Long_vs_Integer.htm
         /// </summary>
-        /// <param name="sketchSegment"></param>
+        /// <param name="sketchPoint"></param>
         /// <returns></returns>
-        private static List<long> GetIds(ISketchSegment sketchSegment)
+        private static List<long> GetIds(ISketchPoint sketchPoint)
         {
             try
             {
                 // Try getting the IDs as integers first, the convert them to longs
-                return ((int[])sketchSegment.GetID()).Select(Convert.ToInt64).ToList();
+                return ((int[])sketchPoint.GetID()).Select(Convert.ToInt64).ToList();
             }
             catch (Exception)
             {
                 // If that fails, try getting them as longs directly
-                return ((long[])sketchSegment.GetID()).ToList();
+                return ((long[])sketchPoint.GetID()).ToList();
             }
         }
 
--- SolidDna/SolidWorks/Sketches/SketchSegmentType.cs
+++ SolidDna/SolidWorks/Sketches/SketchSegmentType.cs
@@ -7,10 +7,6 @@ namespace CADBooster.SolidDna
     /// </summary>
     public enum SketchSegmentType
     {
-        /// <summary>
-        /// Represents a sketch point. Is not present in <see cref="swSketchSegments_e"/>, but we need it for <see cref="SketchSegmentId"/>.
-        /// </summary>
-        Point = -1,
         Line = 0,
         Arc = 1,
         Ellipse = 2,
