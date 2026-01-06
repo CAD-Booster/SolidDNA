4ce0132f057b872d40ff794fcb7430c9c1588491
CADBooster-Peter <peter@cadbooster.com>
2026-01-06 14:28:28 +0100

    Added test project with Microsoft.NET.Test.Sdk for automatic test discovery, NUnit and Moq

---
+++ SolidDna/CADBooster.SolidDna.Test/Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace CADBooster.SolidDna.Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
