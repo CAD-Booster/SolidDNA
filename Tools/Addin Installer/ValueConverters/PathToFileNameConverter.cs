d55d2b7a1f2d8b6de394eb11422b83b67621bf9e
CADBooster-Peter <peter@cadbooster.com>
2026-01-11 21:36:38 +0100

    Updated all templates, tools and tutorials to C# 13.

--- Tools/Addin Installer/ValueConverters/PathToFileNameConverter.cs
+++ Tools/Addin Installer/ValueConverters/PathToFileNameConverter.cs
@@ -3,15 +3,14 @@ using System.Globalization;
 using System.IO;
 using System.Windows.Data;
 
-namespace AngelSix.SolidWorksApi.AddinInstaller.ValueConverters
-{
-    public class PathToFileNameConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-            value != null && value is string path 
-                ? Path.GetFileName(path) 
-                : null;
+namespace AngelSix.SolidWorksApi.AddinInstaller.ValueConverters;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
-    }
+public class PathToFileNameConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value != null && value is string path 
+            ? Path.GetFileName(path) 
+            : null;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
 }
