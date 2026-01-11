6c19130b689483278c94261b775dfd03275d2adc
CADBooster-Peter <peter@cadbooster.com>
2026-01-11 19:35:31 +0100

    Extracted SolidDnaObjectExtensions to its own file. Added tests

--- SolidDna/CADBooster.SolidDna/SolidWorks/Core/SolidDnaObject.cs
+++ SolidDna/CADBooster.SolidDna/SolidWorks/Core/SolidDnaObject.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 
 namespace CADBooster.SolidDna
 {
@@ -89,30 +89,5 @@ namespace CADBooster.SolidDna
         }
 
         #endregion
-    }
-
-    /// <summary>
-    /// Extension method helpers for <see cref="SolidDnaObject{T}"/>
-    /// </summary>
-    public static class SolidDnaObjectExtensions
-    {
-        #region Creation Methods
-
-        /// <summary>
-        /// Checks if the inner COM object is null. If so, returns null instead of 
-        /// the created safe <see cref="SolidDnaObject{T}"/> object
-        /// </summary>
-        /// <typeparam name="T">The type of SolidDnaObject object being created</typeparam>
-        /// <param name="createdObject">The instance that was created</param>
-        /// <returns></returns>
-        public static T CreateOrNull<T>(this T createdObject)
-            where T : SolidDnaObject
-        {
-            return createdObject?.UnsafeObject == null ? null : createdObject;
-        }
-
-
-        #endregion
-
     }
 }
