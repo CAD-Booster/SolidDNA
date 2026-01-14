777e85c74af9aec355244fececf9b11842cc1c9b
CADBooster-Peter <peter@cadbooster.com>
2026-01-14 14:18:05 +0100

    Removed FileHelpers.GetFileFolderName since it has all kinds of strange behavior and does multiple things.

--- SolidDna/CADBooster.SolidDna/File/FileHelpers.cs
+++ SolidDna/CADBooster.SolidDna/File/FileHelpers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace CADBooster.SolidDna;
@@ -10,31 +10,6 @@ public static class FileHelpers
 {
     /// <summary>
     /// Gets a file or folder name from a given path.
-    /// If the path does not contain any folders, the passed in path is returned. 
-    /// Otherwise the file/folder name is returned
-    /// </summary>
-    /// <param name="path">The path to get the file/folder name from</param>
-    /// <returns></returns>
-    public static string GetFileFolderName(string path)
-    {
-        // If we have an empty string, return
-        if (string.IsNullOrEmpty(path))
-            return string.Empty;
-
-        // Normalize slashes
-        var normalizedPath = path.Replace('/', '\\');
-
-        // Get the last slash
-        var lastSlash = normalizedPath.LastIndexOf('\\');
-
-        // If we don't have any folders, return the passed in path
-        if (lastSlash <= 0)
-            return path;
-
-        // Return the last entry of the paths (so the folder/file name)
-        return path.Substring(lastSlash + 1);
-    }
-
     /// <summary>
     /// Gets the codebase directory of a type in a normalized format, removing any file: prefixes
     /// </summary>
