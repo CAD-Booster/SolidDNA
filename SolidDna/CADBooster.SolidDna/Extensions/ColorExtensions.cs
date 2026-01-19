2a1749d8c2965b5a55f0fa3633d9ac4c41626c81
CADBooster-Peter <peter@cadbooster.com>
2026-01-19 16:29:37 +0100

    Added ColorExtensions to convert a color to an integer COLORREF value

---
+++ SolidDna/CADBooster.SolidDna/Extensions/ColorExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace CADBooster.SolidDna;
+
+public static class ColorExtensions
+{
+    /// <summary>
+    /// Convert a System.Windows.Media.Color (WPF color) to a COLORREF integer value.
+    /// Uses r + 256 * g + 65536 * b
+    /// </summary>
+    /// <param name="color">The color to convert.</param>
+    /// <returns>An integer COLORREF value</returns>
+    public static int ToColorRef(this System.Windows.Media.Color color) => color.R + (color.G << 8) + (color.B << 16);
+
+    /// <summary>
+    /// Convert a System.Drawing.Color to a COLORREF integer value.
+    /// </summary>
+    /// <param name="color">The color to convert.</param>
+    /// <returns>An integer COLORREF value.</returns>
+    public static int ToColorRef(this System.Drawing.Color color) => color.R + (color.G << 8) + (color.B << 16);
+}
